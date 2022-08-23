de48516f1376c12e0258649f338646dc93c72e98
Yohane <yohane910@gmail.com>
2022-08-24 03:54:29 +0800

    Thanks Discord.js
    :)  Slash command option limit = 25
    f my life

--- X-Commands/AniEmoji.js
+++ X-Commands/AniEmoji.js
@@ -1,7 +1,12 @@
-const {SlashCommandBuilder} = require("discord.js");
-const { InteractionType } = require('discord.js');
-// const sqlite3 = require('sqlite3')
+const {SlashCommandBuilder, Collection} = require("discord.js");
+// const { InteractionType } = require('discord.js');
+const sqlite3 = require('sqlite3')
+var emoji_Collection = new Collection;
+var emojiname = "";
+var emojiidentifier = "";
+var x = [];
 module.exports = {
+
     data : new SlashCommandBuilder()
         .setName("animoji-ヽowoノ")
         .setDescription("wow")
@@ -12,13 +17,45 @@ module.exports = {
             .setRequired(true),
 
         ),
+
     async autocomplete(client, interaction) {
+        const db = new sqlite3.Database("./lib/Sqlite/X-SQLite.db")
+
+
+        let emojidata = (`SELECT Emoji_Name, Emoji_Identifier FROM ANIMATEDEMOJI`)
+        db.all(emojidata, [], (err, rows) => {
+            if(err) {
+                throw err;
+            }
+                rows.forEach( function fuck(row) {
+                    // console.log(row.Emoji_Name)
+                    emojiname = row.Emoji_Name
+                    emojiidentifier = row.Emoji_Identifier
+                    emoji_Collection.set(emojiname, emojiidentifier)
+                    x.push(emojiname)
+                    console.log(emojiname)
+                    // if(emojiname.length >= 25){
+                    //     console.log("fuck youbitch")
+                    // } else {
+                    //     console.log("fine")
+                    // }
+                    // console.log(emojiname)
+                    // console.log(row.Emoji_Identifier)
+                    }
+                )
+        })
+        // console.log(emojiname)
+        // console.log(emojiidentifier)
+        // console.log(emoji_Collection)
+        // console.log(x)
+
+
         if (interaction.commandName === 'animoji-ヽowoノ') {
             const focusedValue = interaction.options.getFocused();
-            const choices = ['red', 'install', 'collection', 'promise', 'debug'];
-            const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+            // const choices = ['red', 'install', 'collection', 'promise', 'debug'];
+            const filtered = x.filter(x => x.startsWith(focusedValue));
             await interaction.respond(
-                filtered.map(choice => ({ name: choice, value: choice })),
+                filtered.map(x => ({ name: x, value: x })),
             );
         }
 
@@ -26,7 +63,6 @@ module.exports = {
 
     async execute(Client, interaction){
         const option = interaction.options.getString('meow_ヽowoノ');
-        if (interaction.type !== InteractionType.ApplicationCommandAutocomplete) return;
         if (interaction.commandName === "animoji-ヽowoノ") {
         // const interationUser = await interaction.guild.members.fetch(interaction.user.id);
         await interaction.reply({content : `**${option}**表示`})

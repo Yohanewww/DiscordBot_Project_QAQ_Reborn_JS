c92b2548442aa747c51738795ff33290400b1a8b
Yohane <yohane910@gmail.com>
2022-09-01 06:16:23 +0800

    Optimize AniEmoji's code

--- index.js
+++ index.js
@@ -47,33 +47,27 @@ client.on("messageCreate", async (message) => {
         const db = new sqlite3.Database("./lib/Sqlite/X-SQLite.db")
         let emojidata = (`SELECT Emoji_Name, Emoji_Identifier  FROM ANIMATEDEMOJI WHERE AnimatedBoolean = 1`)
         const interationUser = await message.guild.members.fetch(message.author.id);
-        console.log(interationUser)
         db.all(emojidata, [],   (err, rows) => {
-            var emojisName = [];
-            var emojiname = "";
-            var emojiidentifier = "";
-            var emoji_Collection = new Collection;
+            let emojisName = [];
+            let emoji_Collection = new Collection;
             if (err) {
                 throw err;
             }
             rows.forEach(function fuck(row) {
-                    // console.log(row.Emoji_Name)
+                    let emojiidentifier = row.Emoji_Identifier
+                    let emojiname = row.Emoji_Name
                     emoji_Collection.set(emojiname, emojiidentifier)
-                    emojiidentifier = row.Emoji_Identifier
-                    emojiname = row.Emoji_Name
                     emojisName.push(emojiname)
                 },
             )
-            // console.log(emoji_Collection)
-            var message_Content = message.content.slice(PREFIX.length)
+            let message_Content = message.content.slice(PREFIX.length)
             // var FirstContent = message_Content.shift().toLowerCase();
             if (emojisName.includes(message_Content)) {
                 const channel = client.channels.cache.get(message.channelId);
-                var X_emoji = emoji_Collection.get(message_Content)
+                let X_emoji = emoji_Collection.get(message_Content)
                 channel.send({content : `**${interationUser.displayName}**表示`})
                 channel.send("<" + X_emoji + ">")
             }
-
         })
     }
 
